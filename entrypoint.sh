5bbea4947ccd0855cbc1ab1cce620da047e5f05c
Gabriele Gonzato <gabrielegonzato04@gmail.com>
2026-05-25 12:01:49 +0200

    docker configuration working

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 set -e
 
-# 1. Carica l'ambiente ROS standard del container
 source /opt/ros/noetic/setup.bash
 
-# 2. Carica i pacchetti di sistema PAL (versione gallium)
 if [ -f /opt/pal/gallium/setup.bash ]; then
     source /opt/pal/gallium/setup.bash
 fi
 
-# 3. Carica gli eventuali pacchetti custom deployati sul robot
 if [ -f /home/pal/deployed_ws/devel/setup.bash ]; then
     source /home/pal/deployed_ws/devel/setup.bash
 fi
 
-echo "Avvio del server web per l'interfaccia sulla porta 8080..."
+echo "Avvio del server web per l'interfaccia sulla porta 8081..."
 cd /ari_app/display
-python3 -m http.server 8080 &
+python3 -m http.server 8081 &
 
 echo "Avvio della logica di navigazione e gestione ArUco..."
 exec roslaunch /ari_app/scripts/custom_nodes.launch
